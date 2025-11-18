4d28f1ebdb3dbb203406be4c2c650aad8945605c
Niki Wycherley <niki.wycherley@defra.gov.uk>
2025-11-18 16:36:00 +0000

    Fix production path issue - use async readFile in controller

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -1,21 +1,18 @@
-import { readFileSync } from 'fs'
+import { readFile } from 'fs/promises'
+import path from 'path'
 import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-const projectRoot = join(__dirname, '../../..')
-const htmlContent = readFileSync(
-  join(projectRoot, 'public/index.html'),
-  'utf-8'
-)
+const __dirname = path.dirname(__filename)
 
 /**
  * A GDS styled example home page controller.
  * Provided as an example, remove or modify as required.
  */
 export const homeController = {
-  handler(_request, h) {
+  async handler(_request, h) {
+    const filePath = path.join(__dirname, '../../../public/index.html')
+    const htmlContent = await readFile(filePath, 'utf-8')
     return h.response(htmlContent).type('text/html')
   }
 }
